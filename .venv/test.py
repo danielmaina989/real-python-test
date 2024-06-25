6124ac162c3c7dbada4a317936d89108157f297d
Daniel Maina <danielmaina989@gmail.com>
2024-06-25 12:06:43 +0300

    add git setup

---
+++ .venv/test.py
@@ -0,0 +1,23 @@
+# $ git init
+# $ touch README.md
+# $ git add -A
+# $ git commit -m "some message"
+# $ git remote add origin https://github.com/<YOUR-USERNAME>/<YOUR-REPO-NAME>.git
+# $ git push origin master
+
+# after creating the repo;
+
+# $ git add -A
+# $ git commit -am 'message'
+# $ git push origin master
+
+# after making major changes commit them locally and then PUSH to Github.
+
+# $ git add -A
+# $ git commit -am "some message goes here about the commit"
+# $ git push origin master
+
+
+print('Hello World')
+print("first github repo")
+
